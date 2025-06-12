ec14bb8776856cce7ff576388e2c832dd0664177
majd oueslati <majdddin@hotmail.fr>
2025-06-12 18:44:17 +0300

    v7 deployment

--- config.php
+++ config.php
@@ -1,12 +1,16 @@
 <?php
 $host = 'controle-rondement-db.mysql.database.azure.com';
 $dbname = 'task_manager';
-$username = 'majd007'; // Remplacez par votre utilisateur MySQL
-$password = 'vicomteA10!'; // Remplacez par votre mot de passe MySQL
+$username = 'majd007';
+$password = 'vicomteA10!';
+
+$options = [
+    PDO::MYSQL_ATTR_SSL_CA => __DIR__ . '/DigiCertGlobalRootCA.crt.pem',
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+];
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password, $options);
 } catch (PDOException $e) {
     die("Erreur de connexion : " . $e->getMessage());
 }
